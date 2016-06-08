e3b51c1651d179fb154c71be302e947c60b4a643
Lauren Smith <laurensmith@comcast.net>
2016-06-08 15:17:55 -0500

    Added Full French Phrase Translation

--- js/french.js
+++ js/french.js
@@ -1,19 +1,15 @@
 var Translator = (function (wordLife){
-
-  var frenchWords = { "Merry":"Joyeux", "Christmas":"Noel" };
+  var frenchWords = { "Merry":"Joyeux", "Christmas":"Noel", "and":"et", "a":"une", "Happy":"Bonne", "New":"Nouvelle", "Year":"Annee" };
   var translated = "";
 
-    wordLife.toFrench = function (word) {
-      word = word.split(" ");
-      console.log("words: ", word );
-      for (var i = 0; i < word.length; i++) {
-        console.log("word:", i, word[i] );
-        translated += frenchWords[word[i]];
-        translated += " "
-      }
-      return translated;
-    };
+  wordLife.toFrench = function (word) {
+    word = word.split(" ");
+    for (var i = 0; i < word.length; i++) {
+      translated += frenchWords[word[i]];
+      translated += " "
+    }
+    return translated;
+  };
 
   return wordLife;
-
 })(Translator);
