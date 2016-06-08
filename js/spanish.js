a7a34ee4d9328ca52c1e7432b6b79c6d3bd28fa2
Thomas Swann <tkswann@mac.com>
2016-06-08 15:23:19 -0500

    commit before merge

---
+++ js/spanish.js
@@ -0,0 +1,10 @@
+var Translator = (function (oldTranslator) {
+	var lexicon = {"Merry":"", "Christmas":"", "and":"", "Happy":"", "New":"", "Year":""};
+		
+		oldTranslator.translateToSpanish = function (phrase) {
+			phrase = "merry christmas";
+			var phraseArray = phrase.split(' ');
+			console.log(newPhrase);
+		};
+	return oldTranslator;
+})(Translator);
